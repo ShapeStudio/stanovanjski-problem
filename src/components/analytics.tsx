864141d396fbac4e16cb13ade22455a4ae96eef5
Marko <marko.balazic@gmail.com>
2025-03-06 16:10:48 +0100

    umami fix

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -2,7 +2,7 @@ export function Analytics() {
   return (
     <script
       async
-      src="https://analytics.eu.umami.is/script.js"
+      src="https://cloud.umami.is/script.js"
       data-website-id="dd3a12ab-2074-493b-b255-b798c70b4b37"
       data-domains="stanovanjskiproblem.com"
     />
